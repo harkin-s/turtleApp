b9af4f8885c050f52562eb970d14c379c108191b
Sean Harkin <sharkin104@gmail.com>
2017-01-09 10:33:49 +0000

    Working Step Counter

--- app/src/main/java/com/example/gps/app2_gps/CounterActivity.java
+++ app/src/main/java/com/example/gps/app2_gps/CounterActivity.java
@@ -1,13 +1,76 @@
 package com.example.gps.app2_gps;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.hardware.*;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
+import com.google.android.gms.drive.Permission;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.*;
 
-public class CounterActivity extends AppCompatActivity {
+import java.util.Map;
+
+
+public class CounterActivity extends FragmentActivity  implements SensorEventListener{
+
+
+    private SensorManager sensorManager;
+    private TextView count;
+    boolean activityRunning;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counter);
+        count = (TextView) findViewById(R.id.count);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        activityRunning = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (countSensor != null) {
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this, "Count sensor not available!", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        activityRunning = false;
+        // if you unregister the last listener, the hardware will stop detecting step events
+//        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (activityRunning) {
+            count.setText(String.valueOf(event.values[0]));
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 }
---
+++ app/src/main/java/com/example/gps/app2_gps/CounterData.java
@@ -0,0 +1,8 @@
+package com.example.gps.app2_gps;
+
+/**
+ * Created by Sean on 09-Jan-17.
+ */
+
+public class CounterData {
+}
